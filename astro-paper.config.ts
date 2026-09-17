e72df95b8cc0a9846120579cc0cc8fd07df78ec0
faiqbahauddin <faiqbahaa@gmail.com>
2026-09-17 20:05:15 +0700

    Update site URL and title in configuration

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,8 +2,8 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://faiqbahaunddin.github.io/",
-    title: "AstroPaper",
+    url: "https://faiqbahuddin.github.io/",
+    title: "Neraca",
     description: "Making Economic News Easy to Understand.",
     author: "Mochammad Faiq Bahauddin",
     profile: "",
@@ -12,11 +12,13 @@ export default defineAstroPaperConfig({
     timezone: "Asia/Bangkok",
     dir: "ltr",
   },
+
   posts: {
     perPage: 4,
     perIndex: 4,
     scheduledPostMargin: 15 * 60 * 1000,
   },
+
   features: {
     lightAndDarkMode: true,
     dynamicOgImage: true,
@@ -28,18 +30,38 @@ export default defineAstroPaperConfig({
     },
     search: "pagefind",
   },
+
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    {
+      name: "github",
+      url: "https://github.com/satnaing/astro-paper",
+    },
+    {
+      name: "x",
+      url: "https://x.com/username",
+    },
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/faiqbahauddin/",
+    },
+    {
+      name: "mail",
+      url: "mailto:faiqbahauddin@gmail.com",
+    },
   ],
+
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
     { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
-    { name: "x",        url: "https://x.com/intent/post?url=" },
+    { name: "x", url: "https://x.com/intent/post?url=" },
     { name: "telegram", url: "https://t.me/share/url?url=" },
-    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
-    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+    {
+      name: "pinterest",
+      url: "https://pinterest.com/pin/create/button/?url=",
+    },
+    {
+      name: "mail",
+      url: "mailto:?subject=See%20this%20post&body=",
+    },
   ],
 });
